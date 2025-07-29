c3b2e8fba9d4768b63f1d2cdbeba54a8172ada1b
Charles <131194505+Jurai8@users.noreply.github.com>
2025-07-29 21:00:43 +0200

    views 3

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from relationship_app.views import book_list, Library
+from relationship_app.views import book_list, LibraryBookListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
