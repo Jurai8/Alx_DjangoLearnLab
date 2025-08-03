20a759101757c51418847bced73a3d2b6f2c6fac
Charles <131194505+Jurai8@users.noreply.github.com>
2025-08-03 09:07:29 +0200

    Custom model

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
+
 
 # Create your models here.
 
@@ -7,3 +9,21 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField() 
 
+
+class CustomUserManager(models.Manager):
+    # this query set will now be used in the following functions defined here
+    def get_queryset(self):
+        return super().get_queryset()
+    
+    def create_user(self, **kwargs):
+        return self.create(**kwargs)
+    
+    def create_superuser(self, **kwargs):
+        return self.create_superuser(**kwargs)
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    image = models.ImageField(upload_to='image/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
