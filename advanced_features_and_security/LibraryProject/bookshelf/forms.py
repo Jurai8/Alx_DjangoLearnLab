c26bbe23c77e0a53b6bed08d80a0a6d16cb0bd93
Charles <131194505+Jurai8@users.noreply.github.com>
2025-08-04 18:33:05 +0200

    security 2

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,14 @@
+
+from django import forms
+from .models import Book, CustomUser
+from django.contrib.auth.forms import UserCreationForm
+
+
+class CustomUserCreationForm(UserCreationForm):
+    """
+    A form for creating new users. Includes all the fields from
+    AbstractUser, but also handles the password fields correctly.
+    """
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email')
