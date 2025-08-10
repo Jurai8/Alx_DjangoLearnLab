3db5c9af155aa6e24738ef411bc5a9ddbba8fce1
Charles <131194505+Jurai8@users.noreply.github.com>
2025-08-10 10:14:41 +0200

    auth

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,14 +1,18 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from views import BookList, BookViewSet
+from .views import BookList, BookViewSet
 from rest_framework import routers
+from rest_framework.authtoken import views
+
 
 router = routers.DefaultRouter()
 
-router.regiser(r'books_all', BookViewSet, basename='book_all')
+router.register(r'books_all', BookViewSet, basename='book_all')
 
-urls = [
+urlpatterns = [
+    path('api-token-auth/', views.obtain_auth_token),
     path('book/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),
+    
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
-from models import Book
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .models import Book
 from .serializers import BookSerializer
 
 # Create your views here.
 class BookList(generics.ListAPIView):
     "Returns a list of all the books"
+
+    # allow authenticated user to view the books
+    permission_classes = [IsAuthenticated]
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
@@ -15,6 +19,9 @@ class BookViewSet(viewsets.ModelViewSet):
     A viewset that provides the standard actions for a Book model.
     It will handle list, create, retrieve, update, and destroy.
     """
+
+    # allow admin users to perform CRUD operations
+    permission_classes = [IsAdminUser]
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
