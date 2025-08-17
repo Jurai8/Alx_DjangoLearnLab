1d5f2cc5bdf318e32b2ab1b5cce38f71cf28db55
Charles <131194505+Jurai8@users.noreply.github.com>
2025-08-17 08:28:12 +0200

    corrections

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 
 
 
