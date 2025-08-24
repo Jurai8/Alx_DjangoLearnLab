e7368a5a5f8385b6636dfe3f14ef33b3e61e9f0f
Charles <131194505+Jurai8@users.noreply.github.com>
2025-08-24 13:23:26 +0200

    views 4: :

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from views import RegistrationView, LoginView, LogoutView
+from views import RegistrationView, LoginView, LogoutView, ProfileView
 
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('profile/', ProfileView.as_view(template_name='profile.html'), name='profile')
 ]
