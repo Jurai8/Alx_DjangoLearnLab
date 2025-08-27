26f39de0316c6c9933ee9c15c06dfd4236164419
Charles <131194505+Jurai8@users.noreply.github.com>
2025-08-27 16:01:32 +0200

    task 2.1

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('profile/', profile_view, name='profile'),
 
     path('posts/', PostListView.as_view(template_name='all-posts.html'), name='posts'),
-    path('posts/new/', PostCreateView.as_view(template_name='create_posts.html'), name='create_post'),
-    path('posts/<int:pk>/', PostDetailView.as_view(template_name='post_detail.html'), name='post-details'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(template_name='edit_post.html'), name='edit-post'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='delete-post')
+    path('post/new/', PostCreateView.as_view(template_name='create_posts.html'), name='create_post'),
+    path('post/<int:pk>/', PostDetailView.as_view(template_name='post_detail.html'), name='post-details'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(template_name='edit_post.html'), name='edit-post'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='delete-post')
 
 ]
