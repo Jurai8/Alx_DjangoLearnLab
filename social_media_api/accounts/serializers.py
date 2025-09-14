eeb58a61bef86977b19f940268e12962acbd1d18
Charles <131194505+Jurai8@users.noreply.github.com>
2025-09-14 14:59:22 +0200

    task 1.4

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,8 +4,6 @@ from django.contrib.auth import authenticate, get_user_model
 from rest_framework.authtoken.models import Token
 
 
-CustomUser = get_user_model()
-
 class RegistrationSerialzer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
@@ -30,3 +28,26 @@ class RegistrationSerialzer(serializers.ModelSerializer):
             password=validated_data['password']
         )
         return user
+        
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    token = serializers.CharField(read_only=True)
+
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+
+            if user:
+                token, _ = Token.objects.get_or_create(user=user)
+                data['token'] = token.key
+            else:
+                raise serializers.ValidationError('Unable to log in with provided credentials.')
+        else:
+            raise serializers.ValidationError('Must include "username" and "password".')
+
+        return data
